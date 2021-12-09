89fc0542c65481c4b34a594e73dff3fd5952d994
David Dovel <ddovel@uplandsoftware.com>
2021-12-09 17:29:57 -0600

    Refactor game runner into it's own module

---
+++ src/game_runner.rs
@@ -0,0 +1,70 @@
+use rand::prelude::*;
+
+pub trait HuntingAlgorithm
+{
+  fn check(&mut self, upper_bound: i32, rng: &mut ThreadRng) -> i32;
+}
+
+pub struct GameRunner
+{
+  pub max_turns: i32,
+  pub upper_bound: i32,
+  hunter: Box<dyn HuntingAlgorithm>,
+
+  rabbit_position: i32,
+  rng: ThreadRng,
+}
+
+pub struct GameResult {
+  pub did_win: bool,
+  pub num_turns: i32
+}
+
+impl GameRunner
+{
+  pub fn new(hunter: Box<dyn HuntingAlgorithm>, upper_bound: i32, max_turns: i32) -> GameRunner
+  {
+    let mut rng = thread_rng();
+
+    GameRunner {
+      max_turns,
+      upper_bound,
+      hunter,
+      rabbit_position: rng.gen_range(0..upper_bound),
+      rng: rng,
+    }
+  }
+
+  pub fn step(&mut self)
+  {
+    self.rabbit_position = if self.rabbit_position == self.upper_bound {
+      self.rabbit_position - 1
+    } else if self.rabbit_position == 0 {
+      1
+    } else {
+      let direction = [1, -1].iter().choose(&mut self.rng).unwrap();
+      self.rabbit_position + direction
+    }
+  }
+
+  pub fn run(&mut self) -> GameResult
+  {
+    let mut won = false;
+    let mut turn = 0;
+
+    while !won && turn < self.max_turns
+    {
+        turn += 1;
+        self.step();
+        let check = self.hunter.check(self.upper_bound, &mut self.rng);
+        won = check == self.rabbit_position;
+
+        println!("Turn {}, the rabbit moved to {}, and you checked {}", turn, self.rabbit_position, check);
+    }
+
+    GameResult {
+      did_win: won,
+      num_turns: turn,
+    }
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,28 @@
 use rand::prelude::*;
 
+mod game_runner;
+use game_runner::*;
+
 fn main() {
     let max_turns = 1000;
-    let mut game = RabbitGame {
-        upper_bound: 10,
-        rabbit_position: 8,
-    };
-    let mut won = false;
-    let mut turn = 0;
-    let mut rng = thread_rng();
+    let upper_bound = 10;
 
-    let mut algo = RandoAlgo {};
+    let algo = RandoAlgo {};
 
-    while !won && turn < max_turns
-    {
-        turn += 1;
-        game.step(&mut rng);
-        let check = algo.check(game.upper_bound, &mut rng);
-        won = check == game.rabbit_position;
-        println!("Turn {}, the rabbit moved to {}, and you checked {}", turn, game.rabbit_position, check);
+    let mut game = GameRunner::new(Box::new(algo), upper_bound, max_turns);
+    let result = game.run();
+
+    if result.did_win {
+        println!("You won in {} turns!!!", result.num_turns);
+    } else {
+        println!("You lost despite being given {} turns.", result.num_turns);
     }
 }
 
-struct RandoAlgo {
+struct RandoAlgo;
 
-}
-
-impl Algo for RandoAlgo {
+impl HuntingAlgorithm for RandoAlgo {
     fn check(&mut self, upper_bound: i32, rng: &mut ThreadRng) -> i32 {
         rng.gen_range(0..upper_bound)
     }
-}
-
-struct RabbitGame {
-    upper_bound: i32,
-    rabbit_position: i32,
-}
-
-impl RabbitGame {
-    pub fn step(&mut self, rng: &mut ThreadRng)
-    {
-        self.rabbit_position = if self.rabbit_position == self.upper_bound {
-            self.rabbit_position - 1
-        } else if self.rabbit_position == 0 {
-            1
-        } else {
-            self.rabbit_position + [1, -1].iter().choose(rng).unwrap()
-        }
-    }
-}
-
-trait Algo {
-    fn check(&mut self, upper_bound: i32, rng: &mut ThreadRng) -> i32;
 }
