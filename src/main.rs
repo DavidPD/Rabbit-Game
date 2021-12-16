b1769a005b3468bd82c66b8c4b118d7cc47b6654
David Dovel <ddovel@uplandsoftware.com>
2021-12-16 13:46:15 -0600

    Implement a (really bad) repeated sweep algorithm

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let max_turns = 1000;
     let upper_bound = 10;
 
-    let algo = RandoAlgo {};
+    let algo = SweepAlgo { current_position: 0 };
 
     let mut game = GameRunner::new(Box::new(algo), upper_bound, max_turns);
     let result = game.run();
@@ -24,5 +24,20 @@ struct RandoAlgo;
 impl HuntingAlgorithm for RandoAlgo {
     fn check(&mut self, upper_bound: i32, rng: &mut ThreadRng) -> i32 {
         rng.gen_range(0..upper_bound)
+    }
+}
+
+struct SweepAlgo {
+    current_position: i32,
+}
+
+impl HuntingAlgorithm for SweepAlgo {
+    fn check(&mut self, upper_bound: i32, rng: &mut ThreadRng) -> i32 {
+        let check = self.current_position;
+        self.current_position += 1;
+        if self.current_position >= upper_bound {
+            self.current_position = 0;
+        }
+        return check;
     }
 }
