958a552b4438b0361cc8c622b2cd90a8f423ee8f
David Dovel <david.dovel@hey.com>
2022-01-06 23:04:03 -0600

    oops

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn main() {
     let mut rng = thread_rng();
 
     for _ in 0..num_games {
-        let upper_bound = rng.gen_range(10..100);
+        let upper_bound = rng.gen_range(10..=100);
         let max_turns = upper_bound * 2;
 
         let result = play_game::<ConditionalDoubleCheckMax>(upper_bound, max_turns);
